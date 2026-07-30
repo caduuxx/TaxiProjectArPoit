3704de9cac1f4f0f7cf330ca622d745581e408ad
caduuxx <115509827+caduuxx@users.noreply.github.com>
2026-07-30 19:22:54 -0300

    Initial commit

---
+++ src/main/java/com/example/TaxiProjectAriPoint/TaxiProjectAriPointApplication.java
@@ -0,0 +1,13 @@
+package com.example.TaxiProjectAriPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiProjectAriPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiProjectAriPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TaxiProjectAriPoint/TaxiProjectAriPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TaxiProjectAriPoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiProjectAriPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
